eac20b54c3e7e8e9aa8b7a99db86e60d0c27c99c
AmineJ <aminnejawadi@gmail.com>
2020-04-17 02:21:50 +0200

    Frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { FooterComponent } from './componennts/footer/footer.component';
 import { HomeComponent } from './componennts/home/home.component';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { ProductsComponent } from './componennts/products/products.component';
+import { ContactComponent } from './componennts/contact/contact.component';
+import { LoginComponent } from './componennts/login/login.component';
+import { RegisterComponent } from './componennts/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
